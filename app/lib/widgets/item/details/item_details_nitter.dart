f94d0cb8478ac63b3ab2e74e0d8c3c39640c07c9
Rico Berger <ricoberger@users.noreply.github.com>
2023-09-05 21:25:51 +0200

    [nitter] Add Media Gallery to Details View (#2)
    Instead of rendering the images for a Nitter item within the description
    in the details view, we are now rendering the images via the
    "ItemMediaGallery" widget. To not render the images twice, we also added
    the "disableImages" paramter to the "ItemDescription" widget, to not
    render the images in the widget.

--- app/lib/widgets/item/details/item_details_nitter.dart
+++ app/lib/widgets/item/details/item_details_nitter.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 
 import 'package:feeddeck/models/item.dart';
 import 'package:feeddeck/models/source.dart';
+import 'package:feeddeck/utils/constants.dart';
 import 'package:feeddeck/widgets/item/details/utils/item_description.dart';
+import 'package:feeddeck/widgets/item/details/utils/item_media_gallery.dart';
 import 'package:feeddeck/widgets/item/details/utils/item_subtitle.dart';
 import 'package:feeddeck/widgets/item/details/utils/item_title.dart';
 
@@ -33,6 +35,17 @@ class ItemDetailsNitter extends StatelessWidget {
           itemDescription: item.description,
           sourceFormat: DescriptionFormat.html,
           tagetFormat: DescriptionFormat.markdown,
+          disableImages: true,
+        ),
+        const SizedBox(
+          height: Constants.spacingExtraSmall,
+        ),
+        ItemMediaGallery(
+          itemMedias: item.options != null && item.options!.containsKey('media')
+              ? (item.options!['media'] as List)
+                  .map((item) => item as String)
+                  .toList()
+              : null,
         ),
       ],
     );
