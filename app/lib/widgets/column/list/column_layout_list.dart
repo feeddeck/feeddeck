866c62d052ec27d23985d00b34519c5e8523a3bc
ricoberger <mail@ricoberger.de>
2023-10-12 20:40:57 +0200

    [core] Fix Scrolling in List of Sources in Columns
    Above the items of a column we display a list of sources, which can be
    used by a user to only show items for a selected source. If a column
    contains a lot of sources the list is vertical scrollable.
    
    This vertical scrolling is not working on Linux, Windows and the Web and
    only works on touch screens and on the magic trackpad for macOS, because
    at the same time a user can also scroll horizontally.
    
    To fix this we decided to move the list of sources out of the list of
    items on macOS, Linux, Windows and on the web for large screens. This
    way the vertical scroll can not conflict with the horizontal scroll.
    This also means that the sources are always displayed on these
    platforms.
    
    For iOS, Android and on the web for small screens we do not changed the
    behaviour. Since we assume that a user always has a touch screen for the
    scrolling. We can also not change is because we do not have that much
    screen space on these platforms and have to safe the space for the list
    of items.

--- app/lib/widgets/column/list/column_layout_list.dart
+++ app/lib/widgets/column/list/column_layout_list.dart
@@ -1,3 +1,6 @@
+import 'dart:io' show Platform;
+
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 
 import 'package:provider/provider.dart';
@@ -18,6 +21,7 @@ class ColumnLayoutList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     ItemsRepository items = Provider.of<ItemsRepository>(context, listen: true);
+    bool useTabs = MediaQuery.of(context).size.width < Constants.breakpoint;
 
     if (items.column.sources.isEmpty) {
       return Expanded(
@@ -55,29 +59,71 @@ class ColumnLayoutList extends StatelessWidget {
       );
     }
 
-    return Expanded(
-      child: ListView.builder(
-        padding: EdgeInsets.zero,
-        itemCount: items.items.length + 2,
-        itemBuilder: (context, index) {
-          if (index == 0) {
-            return const ColumnLayoutSources(
+    if (kIsWeb && !useTabs ||
+        (!kIsWeb &&
+            (Platform.isLinux || Platform.isMacOS || Platform.isWindows))) {
+      /// On Linux, macOS, Windows and on the web for large screens we display
+      /// the list of sources outside of the list of items. This means that they
+      /// are not part of the list view. This shouldn't be a problem because the
+      /// display should be large enough to always display the sources. This is
+      /// also required to avoid the scrolling bug mentioned in
+      /// https://github.com/feeddeck/feeddeck/issues/25
+      return Expanded(
+        child: Column(
+          children: [
+            const ColumnLayoutSources(
               key: ValueKey('sources'),
-            );
-          }
+            ),
+            Expanded(
+              child: ListView.builder(
+                padding: EdgeInsets.zero,
+                itemCount: items.items.length + 1,
+                itemBuilder: (context, index) {
+                  if (index == items.items.length) {
+                    return const ColumnLayoutLoadMore(
+                      key: ValueKey('loadmore'),
+                    );
+                  }
 
-          if (index == items.items.length + 1) {
-            return const ColumnLayoutLoadMore(
-              key: ValueKey('loadmore'),
-            );
-          }
+                  return ItemPreview(
+                    key: ValueKey(items.items[index].id),
+                    item: items.items[index],
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      );
+    } else {
+      /// On all other platforms, this means on Android, iOS and on the web for
+      /// small screens, the list of sources is part of the list view. We can
+      /// do this because we do not have the scrolling problem on touch screens
+      /// and we save some space on these smaller screens.
+      return Expanded(
+        child: ListView.builder(
+          padding: EdgeInsets.zero,
+          itemCount: items.items.length + 2,
+          itemBuilder: (context, index) {
+            if (index == 0) {
+              return const ColumnLayoutSources(
+                key: ValueKey('sources'),
+              );
+            }
 
-          return ItemPreview(
-            key: ValueKey(items.items[index - 1].id),
-            item: items.items[index - 1],
-          );
-        },
-      ),
-    );
+            if (index == items.items.length + 1) {
+              return const ColumnLayoutLoadMore(
+                key: ValueKey('loadmore'),
+              );
+            }
+
+            return ItemPreview(
+              key: ValueKey(items.items[index - 1].id),
+              item: items.items[index - 1],
+            );
+          },
+        ),
+      );
+    }
   }
 }
