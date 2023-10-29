44547ebb294c116181700871c557520b3ff7bb1e
ricoberger <mail@ricoberger.de>
2023-10-29 21:59:06 +0100

    [youtube] Add Desktop Support
    It is now pissible to play YouTube videos on the native desktop clients.
    To achieve this we removed the "youtube_player_iframe" package which was
    used before to play YouTube video, but which only supported web, iOS and
    Android as target platforms.
    
    On the web we are now using our own implementation to render an iframe
    with for the YouTube video.
    
    On all other platforms we are now using the "youtube_explode_dart"
    package to fetch the video urls for a YouTube video and then we display
    them within our own video player (the "ItemVideoPlayer" widget which was
    added in #51). We also decided to switch the package for the iOS and
    Android implementation which already worked before, because we are now
    able to play YouTube videos in fullscreen and we only have to maintain
    an exception for the web implementation.
    
    The "ItemVideoPlayer" widget now also supports multiple qualities of an
    video via the "qualities" paramter, which allows a user to switch
    between the different video qualities which are available for a YouTube
    video. Last but not least the widget now uses our primary color for the
    seek bar.

---
+++ app/lib/widgets/item/details/utils/item_youtube/item_youtube_video_native.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+import 'package:youtube_explode_dart/youtube_explode_dart.dart';
+
+import 'package:feeddeck/utils/constants.dart';
+import 'package:feeddeck/widgets/item/details/utils/item_media.dart';
+import 'package:feeddeck/widgets/item/details/utils/item_videos.dart';
+import 'item_youtube_video.dart';
+
+class ItemYoutubeVideoNative extends StatefulWidget
+    implements ItemYoutubeVideo {
+  const ItemYoutubeVideoNative({
+    super.key,
+    required this.imageUrl,
+    required this.videoUrl,
+  });
+
+  final String? imageUrl;
+  final String videoUrl;
+
+  @override
+  State<ItemYoutubeVideoNative> createState() => _ItemYoutubeVideoNativeState();
+}
+
+class _ItemYoutubeVideoNativeState extends State<ItemYoutubeVideoNative> {
+  final yt = YoutubeExplode();
+  late Future<List<ItemVideoQuality>> _futureFetchVideoUrls;
+
+  Future<List<ItemVideoQuality>> _fetchVideoUrls() async {
+    final streamManifest = await yt.videos.streamsClient.getManifest(
+      widget.videoUrl,
+    );
+    return streamManifest.muxed
+        .sortByVideoQuality()
+        .map(
+          (element) => ItemVideoQuality(
+            quality: element.qualityLabel,
+            video: element.url.toString(),
+          ),
+        )
+        .toList();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    setState(() {
+      _futureFetchVideoUrls = _fetchVideoUrls();
+    });
+  }
+
+  @override
+  void dispose() {
+    yt.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _futureFetchVideoUrls,
+      builder: (
+        BuildContext context,
+        AsyncSnapshot<List<ItemVideoQuality>> snapshot,
+      ) {
+        if (snapshot.connectionState == ConnectionState.none ||
+            snapshot.connectionState == ConnectionState.waiting ||
+            snapshot.hasError ||
+            snapshot.data == null) {
+          return ItemMedia(itemMedia: widget.imageUrl);
+        }
+
+        return Container(
+          padding: const EdgeInsets.only(
+            bottom: Constants.spacingMiddle,
+          ),
+          child: ItemVideoPlayer(
+            video: snapshot.data!.first.video,
+            qualities: snapshot.data,
+          ),
+        );
+      },
+    );
+  }
+}
+
+ItemYoutubeVideo getItemYoutubeVideo(String? imageUrl, String videoUrl) =>
+    ItemYoutubeVideoNative(
+      imageUrl: imageUrl,
+      videoUrl: videoUrl,
+    );
---
+++ app/lib/widgets/item/details/utils/item_youtube/item_youtube_video_stub.dart
@@ -0,0 +1,6 @@
+import 'item_youtube_video.dart';
+
+ItemYoutubeVideo getItemYoutubeVideo(String? imageUrl, String videoUrl) =>
+    throw UnsupportedError(
+      'Can not ItemYoutubeVideo without the packages dart:html or dart:io',
+    );
