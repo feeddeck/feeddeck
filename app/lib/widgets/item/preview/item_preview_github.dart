891ace2acc2a170d7a72e304cc5143862124e9e6
ricoberger <mail@ricoberger.de>
2023-11-04 11:32:44 +0100

    [github] Fix Icons in Item Preview
    The icons for GitHub items were not shown, because we did not set the
    correct "sourceIconType", which was required for the "ItemSource"
    widget.

--- app/lib/widgets/item/preview/item_preview_github.dart
+++ app/lib/widgets/item/preview/item_preview_github.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:feeddeck/models/item.dart';
 import 'package:feeddeck/models/source.dart';
+import 'package:feeddeck/utils/image_url.dart';
 import 'package:feeddeck/widgets/item/preview/utils/details.dart';
 import 'package:feeddeck/widgets/item/preview/utils/item_actions.dart';
 import 'package:feeddeck/widgets/item/preview/utils/item_description.dart';
@@ -29,6 +30,7 @@ class ItemPreviewGithub extends StatelessWidget {
           sourceSubtitle: '${source.type.toLocalizedString()}: ${source.title}',
           sourceType: source.type,
           sourceIcon: item.media,
+          sourceIconType: FDImageType.item,
           itemPublishedAt: item.publishedAt,
           itemIsRead: item.isRead,
         ),
